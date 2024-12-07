a3b49ec1c63706d22f4e607820319a23e67e9b75
onur <onur.alp.akin@proton.me>
2024-12-07 09:13:49 +0300

    add tree to provider

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/category_model.dart';
+import '../models/category.dart';
 import '../services/category_service.dart';
 
 final categoryServiceProvider = Provider<CategoryService>((ref) {
@@ -8,10 +8,14 @@ final categoryServiceProvider = Provider<CategoryService>((ref) {
 
 final allCategoriesProvider = FutureProvider<List<Category>>((ref) async {
   final service = ref.watch(categoryServiceProvider);
-  return service.getAllCategories();
+  final categories = await service.getAllCategories();
+
+  // Use the static method to build the tree
+  return Category.buildTree(categories);
 });
 
-final categoryByIdProvider = FutureProvider.family<Category, int>((ref, id) async {
+final categoryByIdProvider =
+    FutureProvider.family<Category, int>((ref, id) async {
   final service = ref.watch(categoryServiceProvider);
   return service.getCategoryById(id);
 });
