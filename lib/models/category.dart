5ba312909134d988f552817e56c56283dd9da3fd
onur <onur.alp.akin@proton.me>
2024-12-07 09:10:03 +0300

    add tree structure

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -5,12 +5,14 @@ class Category {
   final String name;
   final int parentId;
   final String? parentName;
+  List<Category> children;
 
   Category({
     required this.id,
     required this.name,
     required this.parentId,
     this.parentName,
+    this.children = const [],
   });
 
   factory Category.fromJson(Map<String, dynamic> json) {
@@ -19,6 +21,7 @@ class Category {
       name: json['name'],
       parentId: json['parentId'],
       parentName: json['parentName'],
+      children: [],
     );
   }
 
@@ -33,5 +36,27 @@ class Category {
   static List<Category> parseCategories(String responseBody) {
     final parsed = json.decode(responseBody)['data'] as List;
     return parsed.map((json) => Category.fromJson(json)).toList();
+  }
+
+  static List<Category> buildTree(List<Category> categories) {
+    final Map<int, List<Category>> groupedByParent = {};
+
+    for (final category in categories) {
+      groupedByParent.putIfAbsent(category.parentId, () => []).add(category);
+    }
+
+    void attachChildren(Category parent) {
+      parent.children = groupedByParent[parent.id] ?? [];
+      for (final child in parent.children) {
+        attachChildren(child);
+      }
+    }
+
+    final List<Category> rootCategories = groupedByParent[0] ?? [];
+    for (final rootCategory in rootCategories) {
+      attachChildren(rootCategory);
+    }
+
+    return rootCategories;
   }
 }
