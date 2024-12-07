f38b9154988e5ca1e185c9a545f7ec3dd47160f6
onur <onur.alp.akin@proton.me>
2024-12-07 14:00:53 +0300

    test the added screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:invmgm_flutter/screens/order_list_screen.dart';
 import 'package:invmgm_flutter/screens/product_list_screen.dart';
 import 'screens/category_tree_screen.dart';
 
@@ -28,7 +29,7 @@ class InvMgmApp extends StatelessWidget {
     return MaterialApp(
       title: 'Category App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ProductListScreen(),
+      home: const OrderListScreen(),
     );
   }
 }
