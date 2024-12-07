6c93b40ae83129769e87a33dc80e6673067425a0
onur <onur.alp.akin@proton.me>
2024-12-07 09:52:20 +0300

    ignore https errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/category_tree_screen.dart';
 
+// ignore certs
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const ProviderScope(child: InvMgmApp()));
 }
 
